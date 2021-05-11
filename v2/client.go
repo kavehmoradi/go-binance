e9723c6b9d872b125e4c8fbdd1d0a9720e194fe8
Kaveh Moradi <kaveh@rabex.co>
2021-05-11 13:08:08 +0430

    Rename module

--- v2/client.go
+++ v2/client.go
@@ -14,9 +14,9 @@ import (
 	"os"
 	"time"
 
-	"github.com/adshao/go-binance/v2/common"
-	"github.com/adshao/go-binance/v2/delivery"
-	"github.com/adshao/go-binance/v2/futures"
+	"github.com/kavehmoradi/go-binance/v2/common"
+	"github.com/kavehmoradi/go-binance/v2/delivery"
+	"github.com/kavehmoradi/go-binance/v2/futures"
 	"github.com/bitly/go-simplejson"
 )
 
